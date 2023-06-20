a4552fc249c169be85ae9a96339c7a022b015575
Michael Amick <michael@defenseunicorns.com>
2023-06-20 17:58:52 -0500

    Completed randomization

--- gotut/hello/hello.go
+++ gotut/hello/hello.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Michael")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Michael")
 	fmt.Println(message)
 }
